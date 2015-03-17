1716903fabab7e040c750929a205b2e2095af97d
Yaogang Lian <ylian@happenapps.com>
2015-03-16 21:08:27 -0400

    Build a framework from the source files.

---
+++ build/GoogleDriveSDK.framework/Headers/GoogleDriveSDK.h
@@ -0,0 +1,68 @@
+//
+//  GoogleDriveSDK.h
+//  GoogleDriveSDK
+//
+//  Created by Yaogang Lian on 3/16/15.
+//  Copyright (c) 2015 HappenApps, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for GoogleDriveSDK.
+FOUNDATION_EXPORT double GoogleDriveSDKVersionNumber;
+
+//! Project version string for GoogleDriveSDK.
+FOUNDATION_EXPORT const unsigned char GoogleDriveSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GoogleDriveSDK/PublicHeader.h>
+#import <GoogleDriveSDK/GTLBase64.h>
+#import <GoogleDriveSDK/GTLBatchQuery.h>
+#import <GoogleDriveSDK/GTLBatchResult.h>
+#import <GoogleDriveSDK/GTLDateTime.h>
+#import <GoogleDriveSDK/GTLDefines.h>
+#import <GoogleDriveSDK/GTLDrive.h>
+#import <GoogleDriveSDK/GTLDriveAbout.h>
+#import <GoogleDriveSDK/GTLDriveApp.h>
+#import <GoogleDriveSDK/GTLDriveAppList.h>
+#import <GoogleDriveSDK/GTLDriveChange.h>
+#import <GoogleDriveSDK/GTLDriveChangeList.h>
+#import <GoogleDriveSDK/GTLDriveChildList.h>
+#import <GoogleDriveSDK/GTLDriveChildReference.h>
+#import <GoogleDriveSDK/GTLDriveConstants.h>
+#import <GoogleDriveSDK/GTLDriveFile.h>
+#import <GoogleDriveSDK/GTLDriveFileList.h>
+#import <GoogleDriveSDK/GTLDriveParentList.h>
+#import <GoogleDriveSDK/GTLDriveParentReference.h>
+#import <GoogleDriveSDK/GTLDrivePermission.h>
+#import <GoogleDriveSDK/GTLDrivePermissionList.h>
+#import <GoogleDriveSDK/GTLDriveRevision.h>
+#import <GoogleDriveSDK/GTLDriveRevisionList.h>
+#import <GoogleDriveSDK/GTLErrorObject.h>
+#import <GoogleDriveSDK/GTLFramework.h>
+#import <GoogleDriveSDK/GTLJSONParser.h>
+#import <GoogleDriveSDK/GTLObject.h>
+#import <GoogleDriveSDK/GTLQuery.h>
+#import <GoogleDriveSDK/GTLQueryDrive.h>
+#import <GoogleDriveSDK/GTLRuntimeCommon.h>
+#import <GoogleDriveSDK/GTLService.h>
+#import <GoogleDriveSDK/GTLServiceDrive.h>
+#import <GoogleDriveSDK/GTLTargetNamespace.h>
+#import <GoogleDriveSDK/GTLUploadParameters.h>
+#import <GoogleDriveSDK/GTLUtilities.h>
+#import <GoogleDriveSDK/GTMGatherInputStream.h>
+#import <GoogleDriveSDK/GTMHTTPFetchHistory.h>
+#import <GoogleDriveSDK/GTMHTTPFetcher.h>
+#import <GoogleDriveSDK/GTMHTTPFetcherLogging.h>
+#import <GoogleDriveSDK/GTMHTTPFetcherService.h>
+#import <GoogleDriveSDK/GTMHTTPUploadFetcher.h>
+#import <GoogleDriveSDK/GTMMIMEDocument.h>
+#import <GoogleDriveSDK/GTMOAuth2Authentication.h>
+#import <GoogleDriveSDK/GTMOAuth2SignIn.h>
+#import <GoogleDriveSDK/GTMOAuth2ViewControllerTouch.h>
+#import <GoogleDriveSDK/GTMOAuth2WindowController.h>
+#import <GoogleDriveSDK/GTMReadMonitorInputStream.h>
+#import <GoogleDriveSDK/GoogleDriveSDK.h>
+#import <GoogleDriveSDK/JSON.h>
+#import <GoogleDriveSDK/SBJsonBase.h>
+#import <GoogleDriveSDK/SBJsonParser.h>
+#import <GoogleDriveSDK/SBJsonWriter.h>
