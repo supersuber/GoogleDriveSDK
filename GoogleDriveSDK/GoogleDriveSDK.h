f6bd559f86dbaf65b2aea9273f226e487b55a2c0
Yaogang Lian <ylian@happenapps.com>
2015-03-16 20:32:12 -0400

    Created a new project.

---
+++ GoogleDriveSDK/GoogleDriveSDK.h
@@ -0,0 +1,19 @@
+//
+//  GoogleDriveSDK.h
+//  GoogleDriveSDK
+//
+//  Created by Yaogang Lian on 3/16/15.
+//  Copyright (c) 2015 HappenApps, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for GoogleDriveSDK.
+FOUNDATION_EXPORT double GoogleDriveSDKVersionNumber;
+
+//! Project version string for GoogleDriveSDK.
+FOUNDATION_EXPORT const unsigned char GoogleDriveSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GoogleDriveSDK/PublicHeader.h>
+
+
